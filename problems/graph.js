62421de5a14ba61293d48c7534970465448615e6
Alejandro Ventura De Moya <alexventura@gmail.com>
2021-04-28 15:05:35 -0500

    work on graph.js

--- problems/graph.js
+++ problems/graph.js
@@ -1,7 +1,7 @@
 
 class Graph {
   constructor() {
-    this.adjList = new Object();
+    this.adjList = {};
   }
 
   addVertex(vertex) { // node
@@ -10,20 +10,26 @@ class Graph {
     }
   }
 
-  addEdges(srcValue, destValue) { // arrows
+  addEdges(srcValue, destValue) { // arrows // srcValue is vertex or node; destValue is a neighbor
     if (!this.adjList[srcValue]) {
       this.addVertex(srcValue)
     }
     if (!this.adjList[destValue]) {
       this.addVertex(destValue)
     }
-    this.adjList[srcValue].push(destValue);
-    this.adjList[destValue].push(srcValue);
+    this.adjList[srcValue].push(destValue); // this.adjList[srcValue] is an array.
+    this.adjList[destValue].push(srcValue); // this.adjList[destValue] is also an array.
   }
 
 
   buildGraph(edges) {
-    // Code goes here ...
+    // edges is a 2d array, e.g.: [["a", b"], ["b", c"]]
+    // this.adjList = {"a": [b], "b": [c]}
+    edges.forEach(pair => {
+      this.addVertex(pair[0]);
+      this.addEdges(pair[0], pair[1]);
+    });
+    return this.adjList;
   }
 
   breadthFirstTraversal(startingVertex) {
